d022d51b814e58409d850a7283a8bd116e1bdab4
jossydee1 <jossydee1@gmail.com>
2022-12-13 14:30:33 +0100

    Desktop View completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Bottom from "./Components/Bottom";
 import Middle from "./Components/Middle";
 import Top from "./Components/Top";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="app">
       <Top />
       <Middle />
+      <Bottom />
     </div>
   );
 }
